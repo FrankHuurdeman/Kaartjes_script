4d708357a59f2504e4357d9792dd2bc030a64b6a
FrankHuurdeman <Frank.Huurdeman@xs4all.nl>
2025-03-11 20:40:16 +0100

    Aanpassingen in python code voor toevoegen Wenen en Nieuwpoort en README geprobeerd aan te passen

--- Kaartjes_script.py
+++ Kaartjes_script.py
@@ -15,6 +15,8 @@ amersfoort_coords = get_coordinates("Amersfoort, Netherlands")
 potsdam_coords = get_coordinates("Potsdam, Germany")
 barcelona_coords = get_coordinates("Barcelona, Spain")
 orebro_coords = get_coordinates("Orebro, Sweden")
+nieuwpoort_coords = get_coordinates("Nieuwpoort, Netherlands")
+vienna_coords = get_coordinates("Vienna, Austria")
 
 # Controleer of de coördinaten correct zijn
 if amersfoort_coords and potsdam_coords and barcelona_coords:
@@ -22,6 +24,8 @@ if amersfoort_coords and potsdam_coords and barcelona_coords:
     print(f"Potsdam: {potsdam_coords}")
     print(f"Barcelona: {barcelona_coords}")
     print(f"Orebro: {orebro_coords}")
+    print(f"Vienna: {vienna_coords}")
+    print(f"Nieuwpoort: {nieuwpoort_coords}")
 
     # Maak een folium kaart
     m = folium.Map(location=[52.0907, 13.2395], zoom_start=6)  # Beginlocatie ergens in Europa
@@ -31,6 +35,8 @@ if amersfoort_coords and potsdam_coords and barcelona_coords:
     folium.Marker(potsdam_coords, popup="Potsdam").add_to(m)
     folium.Marker(barcelona_coords, popup="Barcelona").add_to(m)
     folium.Marker(orebro_coords, popup="Onsliefdesstadje_jwz").add_to(m)
+    folium.Marker(nieuwpoort_coords, popup= "Geboortestadje", icon=folium.Icon(color="blue")).add_to(m)
+    folium.Marker(vienna_coords, popup= "Wenen", icon=folium.Icon(color="red")).add_to(m)
 
     # Sla de kaart op als HTML-bestand
     m.save("index.html")
